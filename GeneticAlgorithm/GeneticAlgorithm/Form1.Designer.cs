3993c6d97bff4b8d2f188b61e5a0f3e52e362ae3
Kirill Shirnin <k.shirnin@gmail.com>
2022-10-12 14:08:57 +0300

    Update Form1.Designer.cs

--- GeneticAlgorithm/GeneticAlgorithm/Form1.Designer.cs
+++ GeneticAlgorithm/GeneticAlgorithm/Form1.Designer.cs
@@ -199,7 +199,7 @@
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(771, 198);
+            this.ClientSize = new System.Drawing.Size(771, 378);
             this.Controls.Add(this.infixPhraseTextBox);
             this.Controls.Add(this.label1);
             this.Controls.Add(this.label7);
